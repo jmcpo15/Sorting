d680ae66f1fc7369822e73625bc4c1d0702e92ee
J McPoland <jm2207@cse070pc-65.cs.york.ac.uk>
2019-02-04 17:23:11 +0000

    04/02/2019

--- InsertionSort.java
+++ InsertionSort.java
@@ -10,31 +10,43 @@ public class InsertionSort {
 		sort(array);
 		//System.out.println("SORT");
 
-		System.out.println("[0, 1, 2,  3,  4, 5, 6, 7]");
+		//System.out.println("[0, 1, 2,  3,  4, 5, 6, 7]");
 		System.out.println(Arrays.toString(array));
 	}
 	
 	
 	public static Integer[] sort(Integer[] arr) {
-
-		if(arr[5] < arr[4]) {
-			System.out.println(place(arr, 5));
-			//insert(arr, slot);
+		for(int i = 1; i < arr.length; i++) {
+			if(arr[i] < arr[i-1]) {
+				int slot = place(arr, i);
+				//insert(arr, i, slot);
+			}
 		}
-		return null;
+		return arr;
 	}
 	
 	public static int place(Integer[] arr, int no) {
-		int find = 1;
+		int find = 0;
 		while(arr[no-find] > arr[no]) {
 			find ++;
 		}
-		
+		System.out.println(no-find+1);
 		return no-find+1;
 	}
 	
+	//Function to insert the chosen number into the correct slot in the array, 
+	//by providing the array, the index the number to be changed and the index which
+	//the number is going to be placed into
 	public static Integer[] insert(Integer[] arr, int init, int slot) {
-		int temp = init;
-		return null;
+		int temp = arr[init];
+		while(init < slot) {
+			System.out.println(init-1);
+			arr[init] = arr[init-1];
+			init --;
+		}
+		if(init == slot) {
+			arr[init] = temp;
+		}
+		return arr;
 	}
 }
