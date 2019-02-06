1bbfc9d0bfc2d5a8e7bcd523f3cb9d93e0a9ae14
Jack McPoland <j.mcpo15@gmail.com>
2019-02-06 01:58:49 +0000

    Start Selection Sort

---
+++ SelectionSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		Integer[] array = new Integer[] {64, 25, 12, 22, 11};
+		
+		sort(array);
+		
+		System.out.println(Arrays.toString(array));
+	}
+
+	
+	public static Integer[] sort(Integer[] array) {
+		return null;
+	}
+	
+	
+	public static void findMin() {
+		
+	}
+	
+	
+	public Integer[] insert() {
+		return null;
+	}
+}
