59da614b15c8dcbd60a7865a7f6ba20e4291c1df
Jack McPoland <j.mcpo15@gmail.com>
2019-02-16 20:23:56 +0000

    Selection Sort finished

--- SelectionSort.java
+++ SelectionSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		Integer[] array = new Integer[] {64, 25, 12, 22, 11};
+		Integer[] array = new Integer[] {64, -25, 12, 22, 11};
 		
 		sort(array);
 		
@@ -12,16 +12,24 @@ public class SelectionSort {
 
 	
 	public static Integer[] sort(Integer[] array) {
-		return null;
-	}
-	
-	
-	public static void findMin() {
 		
+		//Move the boundary each time an element has been sorted
+		for(int i = 0; i < array.length; i++) {
+			
+			//Find the min value
+			int min = i;
+			for(int j = i; j < array.length; j++) {
+				if(array[j] < array[min]) {
+					min = j;
+				}
+			}
+			
+			//Swap the min value with the first element
+			int temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+		}
+		return array;
 	}
 	
-	
-	public Integer[] insert() {
-		return null;
-	}
 }
